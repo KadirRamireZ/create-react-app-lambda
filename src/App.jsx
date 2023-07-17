a8c5ce5aaabf42ac1e0093c1f6c9b0948f01463b
Axl Kadir Ramirez Rodriguez <akramirezr@gmail.com>
2023-07-17 13:14:23 -0600

    add app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
-import "./App.css";
+import React, { useState } from 'react';
+import TaskList from './components/TaskList';
+import AddTask from './components/AddTask';
 
-function App() {
-  
-}
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = text => {
+    const newTask = {
+      id: new Date().getTime().toString(),
+      text,
+      completed: false
+    };
+
+    setTasks(prevTasks => [...prevTasks, newTask]);
+  };
+
+  const deleteTask = id => {
+    setTasks(prevTasks => prevTasks.filter(task => task.id !== id));
+  };
+
+  const toggleTask = id => {
+    setTasks(prevTasks =>
+      prevTasks.map(task =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  return (
+    <div className="app">
+      <h1>Lista de tareas</h1>
+      <AddTask onAdd={addTask} />
+      <TaskList tasks={tasks} onDelete={deleteTask} onToggle={toggleTask} />
+    </div>
+  );
+};
 
 export default App;
+
