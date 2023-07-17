3744e3a5e070ebd08518b827743374d852da89e4
Axl Kadir Ramirez Rodriguez <akramirezr@gmail.com>
2023-07-17 11:18:58 -0600

    add ListTask

--- src/componentes/TaskList/TaskList.js
+++ src/componentes/TaskList/TaskList.js
@@ -1,1 +1,23 @@
+import React from "react";
+import Task from "./Task";
 
+const TaskList = ({ tasks, onDelete, onToggle }) => {
+  return (
+    <div className="task-list">
+      {tasks.length ? (
+        tasks.map((task) => (
+          <Task
+            key={task.id}
+            task={task}
+            onDelete={onDelete}
+            onToggle={onToggle}
+          />
+        ))
+      ) : (
+        <p>No hay tareas</p>
+      )}
+    </div>
+  );
+};
+
+export default TaskList;
